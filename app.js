8ceb0d85956be6a16126f8fd482f51490c7f531f
Tugarik <tugarik@gmail.com>
2023-03-02 23:46:44 +0800

    database generator sql

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import productsRouter from "./routes/product.js";
 import usersRouter from "./routes/user.js";
 import empRouter from "./routes/employee.js";
 
+import fs from 'fs';
+
 const app = express();
 const PORT = 5050;
 
@@ -17,6 +19,34 @@ app.use(empRouter);
 
 app.get("/", (req, res) => {
   res.send("HELLO from Backend");
+});
+
+app.get("/info", (req, res)=>{
+  fs.readFile("./sql/gpt_products.json", (err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+    } else {
+      let savedData = JSON.parse(data);
+      let arr = [];
+      savedData.forEach((el) => {
+        arr = [...arr, [
+          el.name, 
+          el.description,
+          el.price,
+          el.image_url,
+          el.category,
+          el.brand,
+          el.rating,
+          el.reviews,
+          el.stock,
+          el.sale,
+          el.createdBy,
+          el.createdDate
+        ]];
+        console.log(arr);    
+      });
+    }
+  });
 });
 
 app.listen(PORT, () => {
